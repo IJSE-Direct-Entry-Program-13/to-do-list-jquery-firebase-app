1e743acb91d002ce2f9d7a6c9f5f40ffbc010e82
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-03-04 16:25:57 +0530

    Implement auth stage - 1

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,3 +1,18 @@
 import {auth} from './firebase.config.js';
+import $ from 'jquery';
+import {GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 
-console.log(auth);
+const provider = new GoogleAuthProvider();
+
+$("#btn-sign-in-google").on('click', async () => {
+    try {
+        const result = await signInWithPopup(auth, provider);
+        console.log(result);
+        $("#login").addClass("d-none");
+        $("#app").removeClass("d-none");
+    }catch (e){
+        $("#login").removeClass("d-none");
+        $("#app").addClass("d-none");
+        console.log(e);
+    }
+});
